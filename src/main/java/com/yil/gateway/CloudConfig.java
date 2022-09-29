4fadc73f0e6656a47e48ef2ce952e587e540ab40
omursoykinay <btkpostman23@gmail.com>
2022-09-29 10:53:45 +0300

    added

--- src/main/java/com/yil/gateway/CloudConfig.java
+++ src/main/java/com/yil/gateway/CloudConfig.java
@@ -13,6 +13,8 @@ public class CloudConfig {
         return builder
                 .routes()
                 .route("personModule", r -> r.path("/api/prs/v1/**").uri("http://localhost:8083/"))
+                .route("educationModule", r -> r.path("/api/edu/v1/**").uri("http://localhost:8088/"))
+                .route("jobModule", r -> r.path("/api/job/v1/**").uri("http://localhost:8089/"))
                 .build();
     }
 
